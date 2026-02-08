98ff755e307b9f670cdc7f91da337fdd896e6b54
Roman <yummison44@gmail.com>
2026-02-08 22:32:54 +0100

    design stuff

--- src/main/java/easv/event_tickets_easv_prototype/gui/GreetWindowController.java
+++ src/main/java/easv/event_tickets_easv_prototype/gui/GreetWindowController.java
@@ -1,14 +1,60 @@
 package easv.event_tickets_easv_prototype.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GreetWindowController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button eventCoordinatorButton;
+
+    @FXML
+    private Button adminButton;
+
+    @FXML
+    private boolean admin;
+
+    private Scene previousScene;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private void loginClick(ActionEvent event){
+        if (event.getSource() == adminButton){
+            admin = true;
+        }
+        try {
+            openLoginWindow();
+        }
+        catch (IOException e){
+            System.out.println("error");
+        }
+
+    }
+
+    private void openLoginWindow() throws IOException {
+        FXMLLoader loader = new FXMLLoader(GreetWindowController.class.getResource("/easv/event_tickets_easv_prototype/gui/login_view.fxml"));
+        VBox parent = (VBox) loader.load();
+        Stage stage = (Stage) adminButton.getScene().getWindow();
+        stage.setScene(new Scene(parent));
+    }
+
+    @FXML
+    private void actualLoginClick(){
+
     }
 }

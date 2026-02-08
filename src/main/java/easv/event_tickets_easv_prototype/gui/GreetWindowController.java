abc5476c866aac0b02fa12c8eb212c0d3c6a3d35
ArtemSapehin <minethegame4@gmail.com>
2026-02-08 23:23:53 +0100

    added coordinator fxml

--- src/main/java/easv/event_tickets_easv_prototype/gui/GreetWindowController.java
+++ src/main/java/easv/event_tickets_easv_prototype/gui/GreetWindowController.java
@@ -20,9 +20,6 @@ public class GreetWindowController {
     private Button eventCoordinatorButton;
 
     @FXML
-    private Button loginButton;
-
-    @FXML
     private Button adminButton;
 
     @FXML
@@ -44,49 +41,16 @@ public class GreetWindowController {
             openLoginWindow();
         }
         catch (IOException e){
-            System.out.println("error while opening login window");
+            System.out.println("error");
         }
 
-    }
-
-    private void setAdmin(boolean bool){
-        this.admin = bool;
     }
 
     private void openLoginWindow() throws IOException {
-        if (admin){
-            FXMLLoader loader = new FXMLLoader(GreetWindowController.class.getResource("/easv/event_tickets_easv_prototype/gui/login_view.fxml"));
-            VBox parent = (VBox) loader.load();
-            GreetWindowController gwc = loader.getController();
-            gwc.setAdmin(true);
-            Stage stage = (Stage) adminButton.getScene().getWindow();
-            stage.setScene(new Scene(parent));
-        }
-        else{
-            FXMLLoader loader = new FXMLLoader(GreetWindowController.class.getResource("/easv/event_tickets_easv_prototype/gui/login_view.fxml"));
-            VBox parent = (VBox) loader.load();
-            Stage stage = (Stage) adminButton.getScene().getWindow();
-            stage.setScene(new Scene(parent));
-        }
-
-    }
-
-    @FXML
-    private void actualLoginIn() {
-        if (admin){
-            try {
-                FXMLLoader loader = new FXMLLoader(GreetWindowController.class.getResource("/easv/event_tickets_easv_prototype/gui/admin_panel.fxml"));
-                VBox parent = (VBox) loader.load();
-                Stage stage = (Stage) loginButton.getScene().getWindow();
-                stage.setScene(new Scene(parent));
-            }
-            catch (IOException e){
-                System.out.println("error");
-            }
-        }
-        else{
-            System.out.println("pashol nahuy");
-        }
+        FXMLLoader loader = new FXMLLoader(GreetWindowController.class.getResource("/easv/event_tickets_easv_prototype/gui/login_view.fxml"));
+        VBox parent = (VBox) loader.load();
+        Stage stage = (Stage) adminButton.getScene().getWindow();
+        stage.setScene(new Scene(parent));
     }
 
     @FXML
